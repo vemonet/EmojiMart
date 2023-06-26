6ec82602bdfdabe4463a4e9ae916f3259a86d142
Vincent Emonet <vincent.emonet@gmail.com>
2023-06-26 14:14:40 +0200

    pop window in the middle, fix window height, enable autofocus and dynamic width. Still need to improve height handling

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
-# Tauri + Solid + Typescript
+# 🏪 Emoji Mart app
 
-This template should help get you started developing with Tauri, Solid and Typescript in Vite.
+Built with Tauri, Solid JS, and Typescript using Vite.
+
+Install:
 
 ```bash
-yarn dev
+yarn
+```
+
+Run in development:
+
+```bash
+yarn tauri dev
+```
+
+Build:
+
+```bash
+yarn tauri build
 ```
 
 ## Recommended IDE Setup
 
-- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+[VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+
+## Notes
+
+Creating a system tray and running in the background: https://tauri.app/fr/v1/guides/features/system-tray/
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import data from "@emoji-mart/data";
-// import Picker from "@emoji-mart/react";
 import { Picker } from 'emoji-mart'
-import { clipboard } from "@tauri-apps/api";
+import { clipboard, window } from "@tauri-apps/api";
+// import { useKeyDownEvent } from "@solid-primitives/keyboard";
 
 export interface EmojiData {
   // only some properties, waiting for https://github.com/missive/emoji-mart/pull/789
@@ -15,25 +15,23 @@ export interface EmojiData {
 
 function App() {
   const onEmojiSelect = (emoji: EmojiData) => {
-    console.log('IN EMOJI SELECT')
     clipboard.writeText(emoji.native);
+    setTimeout(() => window.appWindow.close(), 0);
   };
 
-  console.log('CREATE PICKER')
-  return new Picker({ data, onEmojiSelect })
+  // TODO: close window when click outside or click ESC?
+  // const event = useKeyDownEvent();
+  // const e = event();
+  // if (e) {
+  //   console.log(e.key); // => "Q" | "ALT" | ... or null
+  //   if (e.key === "ALT") {
+  //     window.appWindow.close()
+  //   }
+  //   e.preventDefault(); // prevent default behavior or last keydown event
+  // }
 
-  // return (
-  //   <div class="container">
-  //     <div class="picker">
-  //       <Picker
-  //         data={data}
-  //         onEmojiSelect={onEmojiSelect}
-  //         autoFocus={true}
-  //         dynamicWidth={true}
-  //       />
-  //     </div>
-  //   </div>
-  // );
+  return new Picker({ data, onEmojiSelect, autoFocus: true, dynamicWidth: true})
+
 }
 
 export default App;
