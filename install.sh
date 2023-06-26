563bbe042cf2d7d74d8164abfec22cef74eb4fb3
Vincent Emonet <vincent.emonet@gmail.com>
2023-06-26 18:06:05 +0200

    improve appimage install

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-
+# Script to install the Emoji Mart AppImage as desktop application on linux
 APPIMAGE_PATH="/home/vemonet/develop/perso/EmojiMart/src-tauri/target/release/bundle/appimage/emoji-mart-app_0.1.0_amd64.AppImage"
+VERSION="0.1.0"
 
+# Download:
+# wget -O ~/.local/bin/EmojiMart.AppImage https://github.com/vemonet/EmojiMart/releases/download/$VERSION/emoji-mart-app_$VERSION_amd64.AppImage
+# wget -O ~/.local/share/applications/EmojiMart.desktop https://github.com/vemonet/EmojiMart/blob/main/public/EmojiMart.desktop?raw=true
+
+# Copy from local:
 cp $APPIMAGE_PATH ~/.local/bin/EmojiMart.AppImage
 cp public/EmojiMart.desktop ~/.local/share/applications/
-
-
-# flatpak run io.github.vemonet.EmojiMart
-
-# flatpak-builder --ccache --force-clean _build io.github.vemonet.EmojiMart.yml --repo=_repo
-# flatpak build-bundle _repo io.github.vemonet.EmojiMart.flatpak io.github.vemonet.EmojiMart
