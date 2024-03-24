542b6673d154d7326ce11ca68abc14ad34ee4ed8
Vincent Emonet <vincent.emonet@gmail.com>
2024-03-24 10:50:11 +0100

    Upgrade dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
 import adapter from '@sveltejs/adapter-static'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// cf. https://kit.svelte.dev/docs/integrations#preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter()
 	}
